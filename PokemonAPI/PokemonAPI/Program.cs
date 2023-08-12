c1e04870f146c1bcc5111265372f5608de441737
JotaPeAyres <jp.ayres2601@gmail.com>
2023-08-12 14:44:37 -0300

    Method get to bring a list of pokemon

---
+++ PokemonAPI/PokemonAPI/Program.cs
@@ -0,0 +1,34 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonAPI
+{
+    class Program
+    {
+        static void Main()
+        {
+            GetAllPokemon();
+        }
+
+        public static void GetAllPokemon()
+        {
+            var options = new RestClientOptions("https://pokeapi.co/api/v2/");
+
+            var client = new RestClient(options);
+            var request = new RestRequest("pokemon/", Method.Get);
+
+            var response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                Console.WriteLine(response.Content);
+            else
+                Console.WriteLine(response.ErrorMessage);
+
+            Console.ReadKey();
+        }
+    }
+}
