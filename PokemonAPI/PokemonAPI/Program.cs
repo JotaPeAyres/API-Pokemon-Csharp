97628bb235b01f0882471d560b28fbebe2e639ee
JotaPeAyres <jp.ayres2601@gmail.com>
2023-08-12 15:26:54 -0300

    Showing pokemon basic information

--- PokemonAPI/PokemonAPI/Program.cs
+++ PokemonAPI/PokemonAPI/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace PokemonAPI
@@ -19,16 +20,57 @@ namespace PokemonAPI
             var options = new RestClientOptions("https://pokeapi.co/api/v2/");
 
             var client = new RestClient(options);
-            var request = new RestRequest("pokemon/", Method.Get);
+            var request = new RestRequest("pokemon/?limit=5", Method.Get);
 
             var response = client.Execute(request);
 
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
-                Console.WriteLine(response.Content);
+            {
+                var results = JsonSerializer.Deserialize<ResponseJson>(response.Content);
+
+                results.results.ForEach(f =>
+                {
+                    var pokemonRequest = new RestRequest($"pokemon/{f.name}", Method.Get);
+                    var pokemonResponse = JsonSerializer.Deserialize<Pokemon>(client.Execute(pokemonRequest).Content);
+                    Console.WriteLine("Pokemon name: " + pokemonResponse.name);
+                    Console.WriteLine("Height: " + pokemonResponse.height);
+                    Console.WriteLine("Weight: " + pokemonResponse.weight);
+                    //Console.WriteLine("Abilities: ");
+                    //pokemonResponse.abilities.ForEach(a => { Console.WriteLine("\t" + a.name); });
+                });
+            }
+
             else
                 Console.WriteLine(response.ErrorMessage);
 
             Console.ReadKey();
+        }
+
+        public class ResponseJson
+        {
+            public int count { get; set; }
+            public string next { get; set; }
+            public string previous { get; set; }
+            public List<Result> results { get; set; }
+
+            public class Result
+            {
+                public string name { get; set; }
+                public string url { get; set; }
+            }
+        }
+
+        public class Pokemon
+        {
+            public string name { get; set; }
+            public int height { get; set; }
+            public int weight { get; set; }
+            public List<Abilities> abilities { get; set; }
+        }
+
+        public class Abilities
+        {
+            public string name { get; set; }
         }
     }
 }
